956ab76d4cc37a96b64954c6f6eb271c5c301393
Jakhongir Farkhodov <jahongirfarxodov@gmail.com>
2025-01-09 23:19:33 +0500

    Commit

---
+++ src/ТАЖИКУЛОВ_САРДОРБЕК_РАВШАНБЕКОВИЧ/Main.java
@@ -0,0 +1,9 @@
+package ТАЖИКУЛОВ_САРДОРБЕК_РАВШАНБЕКОВИЧ;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
